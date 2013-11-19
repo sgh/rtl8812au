0becfe3c1c063f2125c7f942c799b55c2bda4745
Søren Holm <sgh@sgh.dk>
2013-11-19 22:23:05 +0100

    Skip setting up proc-entries on linux >= 3.10
    Proc-api has changed. So just disable it for now.

--- os_dep/linux/os_intfs.c
+++ os_dep/linux/os_intfs.c
@@ -318,6 +318,7 @@ static int	rtw_proc_cnt = 0;
 
 void rtw_proc_init_one(struct net_device *dev)
 {
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(3,10,0))
 	struct proc_dir_entry *dir_dev = NULL;
 	struct proc_dir_entry *entry=NULL;
 	_adapter	*padapter = rtw_netdev_priv(dev);
@@ -652,7 +653,7 @@ void rtw_proc_init_one(struct net_device *dev)
 	}
 	entry->write_proc = proc_set_sreset;
 #endif /* DBG_CONFIG_ERROR_DETECT */
-
+#endif
 }
 
 void rtw_proc_remove_one(struct net_device *dev)
